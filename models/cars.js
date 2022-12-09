e19aa2d54e5e47b57f94ac9dc15e88b94ddc2110
Corey Hall <cohall15@gmail.com>
2022-12-08 19:15:16 -0500

    added car module

--- models/cars.js
+++ models/cars.js
@@ -1,17 +1,129 @@
-Car = [
+module.exports = [
     {
       id: "001",
-      name: "Bulbasaur",
-      img: "http://img.pokemondb.net/artwork/bulbasaur.jpg",
+      name: "2022 Toyota Camry",
+      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
       type: [
-        "Grass",
-        "Poison"
+        "Sedan",
+        
       ],
       stats: {
-        price: "45",
+        price: "45 a day",
         
       },
-    }
-    ]
+    } ,  
+    {
+      id: "001",
+      name: "2022 Toyota Camry",
+      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      type: [
+        "Sedan",
+        
+      ],
+      stats: {
+        price: "45 a day",
+        
+      },
+    } ,  
+
+
+    {
+      id: "001",
+      name: "2022 Toyota Camry",
+      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      type: [
+        "Sedan",
+        
+      ],
+      stats: {
+        price: "45 a day",
+        
+      },
+    } ,
+    
+    {
+      id: "001",
+      name: "2022 Toyota Camry",
+      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      type: [
+        "Sedan",
+        
+      ],
+      stats: {
+        price: "45 a day",
+        
+      },
+    }  , 
+
+    {
+      id: "001",
+      name: "2022 Toyota Camry",
+      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      type: [
+        "Sedan",
+        
+      ],
+      stats: {
+        price: "45 a day",
+        
+      },
+    },  
+
+    {
+      id: "001",
+      name: "2022 Toyota Camry",
+      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      type: [
+        "Sedan",
+        
+      ],
+      stats: {
+        price: "45 a day",
+        
+      },
+    } ,  
+
+    {
+      id: "001",
+      name: "2022 Toyota Camry",
+      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      type: [
+        "Sedan",
+        
+      ],
+      stats: {
+        price: "45 a day",
+        
+      },
+    } ,  
+
+    {
+      id: "001",
+      name: "2022 Toyota Camry",
+      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      type: [
+        "Sedan",
+        
+      ],
+      stats: {
+        price: "45 a day",
+        
+      },
+    } ,  
+
+    {
+      id: "001",
+      name: "2022 Toyota Camry",
+      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      type: [
+        "Sedan",
+        
+      ],
+      stats: {
+        price: "45 a day",
+        
+      },
+    } ,  
+  ]
 
     
