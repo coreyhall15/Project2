2b6459678cbeffc2e653d07c49f860ea68079440
Corey Hall <cohall15@gmail.com>
2022-12-10 09:45:31 -0500

    added photos of cars

--- models/cars.js
+++ models/cars.js
@@ -2,125 +2,125 @@ module.exports = [
     {
       id: "001",
       name: "2022 Toyota Camry",
-      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      img: "https://vehiclephotos.b-cdn.net/photos/vehicles/239862/4090698-small.jpg",
       type: [
         "Sedan",
         
       ],
       stats: {
-        price: "45 a day",
+        price: "$68 a day",
         
       },
     } ,  
     {
-      id: "001",
-      name: "2022 Toyota Camry",
-      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      id: "002",
+      name: "2022 Corvette C8",
+      img: "https://cars.usnews.com/static/images/Auto/izmo/i159614411/2021_chevrolet_corvette_stingray_angularfront.jpg",
       type: [
         "Sedan",
         
       ],
       stats: {
-        price: "45 a day",
+        price: "$300 a day",
         
       },
     } ,  
 
 
     {
-      id: "001",
-      name: "2022 Toyota Camry",
-      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      id: "003",
+      name: "2023 Jeep Wagoneer",
+      img: "https://www.gannett-cdn.com/presto/2021/10/19/NPRJ/09dc1cbe-d053-4667-bce3-3e5513031d54-01jeep.jpg?width=660&height=403&fit=crop&format=pjpg&auto=webp",
       type: [
-        "Sedan",
+        "SUV",
         
       ],
       stats: {
-        price: "45 a day",
+        price: "$99 a day",
         
       },
     } ,
     
     {
-      id: "001",
-      name: "2022 Toyota Camry",
-      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      id: "004",
+      name: "2022 Cadillac Escalade",
+      img: "https://images.dealer.com/ddc/vehicles/2022/CADILLAC/Escalade%20ESV/SUV/color/Black%20Raven-GBA-5,5,7-640-en_US.jpg",
       type: [
-        "Sedan",
+        "SUV",
         
       ],
       stats: {
-        price: "45 a day",
+        price: "$250 a day",
         
       },
     }  , 
 
     {
-      id: "001",
-      name: "2022 Toyota Camry",
-      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      id: "005",
+      name: "2022 Mercedes E Class Coupe",
+      img: "https://images.dealer.com/autodata/us/640/color/2022/USD20MBC893A0/149.jpg",
       type: [
         "Sedan",
         
       ],
       stats: {
-        price: "45 a day",
+        price: "$160 a day",
         
       },
     },  
 
     {
-      id: "001",
-      name: "2022 Toyota Camry",
-      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      id: "006",
+      name: "2021 Dodge Charger Hellcat",
+      img: "https://static.kbb.ca/pkw/p/2022DOC200119_01.jpg",
       type: [
         "Sedan",
         
       ],
       stats: {
-        price: "45 a day",
+        price: "$200 a day",
         
       },
     } ,  
 
     {
-      id: "001",
-      name: "2022 Toyota Camry",
-      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      id: "007",
+      name: "2022 Dodge Durango",
+      img: "https://images.dealer.com/autodata/us/color/2022/USD20DOS012C0/PDN.jpg?impolicy=downsize_bkpt&imdensity=1&w=520",
       type: [
         "Sedan",
         
       ],
       stats: {
-        price: "45 a day",
+        price: "$105 a day",
         
       },
     } ,  
 
     {
-      id: "001",
-      name: "2022 Toyota Camry",
-      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      id: "008",
+      name: "2022 Chevy Tahoe",
+      img: "https://images.dealer.com/ddc/vehicles/2022/Chevrolet/Tahoe/SUV/trim_LS_8f3264/color/Auburn%20Metallic-G48-82,48,41-640-en_CA.jpg",
       type: [
-        "Sedan",
+        "SUV",
         
       ],
       stats: {
-        price: "45 a day",
+        price: "$135 a day",
         
       },
     } ,  
 
     {
-      id: "001",
-      name: "2022 Toyota Camry",
-      img: "https://pictures.dealer.com/n/nalleytoyotascionofroswell/0383/2814f8d9a521d3abe99676d833e8b8f3x.jpg?impolicy=resize&w=768",
+      id: "009",
+      name: "2022 Nissan Titan Pickup",
+      img: "https://cars.usnews.com/pics/size/350x262/images/Auto/izmo/i159615023/2022_nissan_titan_xd_angularfront.jpg",
       type: [
-        "Sedan",
+        "Truck",
         
       ],
       stats: {
-        price: "45 a day",
+        price: "$89 a day",
         
       },
     } ,  
